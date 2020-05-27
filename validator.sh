bb8535c356bfdc8a145500db8aee1a4edff737e6
sych74 <sych74@gmail.com>
2020-05-27 19:48:59 +0300

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -2,7 +2,7 @@
 IFS=',' read -r -a nodes <<< "$1"
 for node in "${nodes[@]}"
 do
-  NETPATH=`traceroute -m4 -n $node 2>/dev/null | awk '{print $2}' | grep -v to`
-  [[ `echo $NETPATH` =~ ${0##*/} ]] && echo "VALIDATOR ERROR: Failed connect to " `host ${node}` ": Traceroute out: ${NETPATH} "
+  NETPATH=`tracepath -m4 -n $node 2>/dev/null | awk '{print $2}' | grep -v to`
+  [[ `echo $NETPATH` =~ "no " ]] && echo "VALIDATOR ERROR: Failed connect to " `host ${node}` ": Traceroute out: ${NETPATH} "
 done
 exit 0
