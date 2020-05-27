1a82067107348dd090a0333923f4e0251a588bc7
sych74 <sych74@gmail.com>
2020-05-27 19:12:04 +0300

    Create validator.sh

---
+++ validator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+IFS=',' read -r -a nodes <<< "$1"
+for node in "${nodes[@]}"
+do
+  NETPATH=`traceroute -m4 -w3 -n $node 2>/dev/null | awk '{print $2}' | grep -v to`
+  [[ `echo $NETPATH` =~ ${0##*/} ]] && echo "VALIDATOR ERROR: Failed connect to "host ${node}": Traceroute out: ${NETPATH} "
+done
